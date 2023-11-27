e4b9f31607dd6e4a5971ad2e476ab2bac0afd09c
szerszen20 <kamilboron28@gmail.com>
2023-11-27 12:48:59 +0100

    added activeDelivery in js

--- script.js
+++ script.js
@@ -43,3 +43,49 @@ function openbackground() {
     document.getElementById("now").style.display = "none";
     document.body.style.overflow = "auto";
   }
+
+
+  window.onscroll = function()
+  {
+    activeDelivery()
+  }
+
+  function activeDelivery() {
+    let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+ 
+    let delivery = document.getElementById('delivery');
+    let field = document.getElementById('field');
+    let fix = document.getElementById("delivery-fix");
+ 
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      delivery.style.display = "fixed";
+      delivery.style.top = "0px";
+      delivery.style.left = "0px";
+      delivery.style.height = "72px";
+      delivery.style.width = "100%";
+      delivery.style.backgroundColor= "white";
+      delivery.style.padding= "8px 16px";
+      field.style.backgroundColor = "rgb(238, 238, 238)";
+      console.log("if if if");
+
+    }  else {
+      field.style.position = "sticky";
+      delivery.style.padding= "8px 0px";
+      delivery.style.height = "56px";
+      delivery.style.backgroundColor= "unset";
+      delivery.style.position = "sticky";
+      delivery.style.cursor = "pointer";
+      delivery.style.padding = "8px 0";
+      delivery.style.display = "block";
+      delivery.style.zIndex = "3";
+
+
+      field.style.backgroundColor = "#FFFFFF";
+      console.log("else");
+    }
+  }
+
+
+
+  
+  window.addEventListener("scroll", activeDelivery);
