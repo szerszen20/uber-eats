58d4f66138ee87616cb47d9893aa7818d4d721dd
szerszen20 <kamilboron28@gmail.com>
2023-11-28 10:49:39 +0100

    fixed activeDelivery

--- script.js
+++ script.js
@@ -57,30 +57,23 @@ function openbackground() {
     let field = document.getElementById('field');
     let fix = document.getElementById("delivery-fix");
  
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-      delivery.style.display = "fixed";
-      delivery.style.top = "0px";
-      delivery.style.left = "0px";
-      delivery.style.height = "72px";
-      delivery.style.width = "100%";
-      delivery.style.backgroundColor= "white";
-      delivery.style.padding= "8px 16px";
-      field.style.backgroundColor = "rgb(238, 238, 238)";
+    if (document.body.scrollTop > 210 || document.documentElement.scrollTop > 210) {
+      delivery.classList.remove("delivery-address");
+      field.classList.remove("delivery-address-field");
+      fix.classList.remove("delivery-address-fixed");
+
+      delivery.classList.add("delivery-active");
+      field.classList.add("field-active");
+      fix.classList.add("fixed-active");
       console.log("if if if");
+    } else {
+      delivery.classList.add("delivery-address");
+      field.classList.add("delivery-address-field");
+      fix.classList.add("delivery-address-fixed");
 
-    }  else {
-      field.style.position = "sticky";
-      delivery.style.padding= "8px 0px";
-      delivery.style.height = "56px";
-      delivery.style.backgroundColor= "unset";
-      delivery.style.position = "sticky";
-      delivery.style.cursor = "pointer";
-      delivery.style.padding = "8px 0";
-      delivery.style.display = "block";
-      delivery.style.zIndex = "3";
-
-
-      field.style.backgroundColor = "#FFFFFF";
+      delivery.classList.remove("delivery-active");
+      field.classList.remove("field-active");
+      fix.classList.remove("fixed-active");
       console.log("else");
     }
   }
