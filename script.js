c39b8cf85bf20957af0cb2422f8ab5c28101dbf8
szerszen20 <kamilboron28@gmail.com>
2023-11-30 13:04:21 +0100

    added navbar animation and fixed hamburger-menu

--- script.js
+++ script.js
@@ -36,40 +36,63 @@
   }
 
 
+
   window.onscroll = function()
   {
-    activeDelivery()
-  }
 
-  function activeDelivery() {
-    let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
- 
-    let delivery = document.getElementById('delivery');
-    let field = document.getElementById('field');
-    let fix = document.getElementById("delivery-fix");
- 
-    if (document.body.scrollTop > 210 || document.documentElement.scrollTop > 210) {
-      delivery.classList.remove("delivery-address");
-      field.classList.remove("delivery-address-field");
-      fix.classList.remove("delivery-address-fixed");
+    checkScreenWidth();
 
-      delivery.classList.add("delivery-active");
-      field.classList.add("field-active");
-      fix.classList.add("fixed-active");
-      console.log("if if if");
-    } else {
-      delivery.classList.add("delivery-address");
-      field.classList.add("delivery-address-field");
-      fix.classList.add("delivery-address-fixed");
-
-      delivery.classList.remove("delivery-active");
-      field.classList.remove("field-active");
-      fix.classList.remove("fixed-active");
-      console.log("else");
-    }
   }
 
 
 
+    checkScreenWidth();
+
+   
+
+    function checkScreenWidth() {
+
+      let screenWidth = window.innerWidth;
+      let navbar = document.getElementById("navbar");
+      let delivery = document.getElementById('delivery');
+      let field = document.getElementById('field');
+      let fix = document.getElementById("delivery-fix");
+   
+      if (screenWidth < 700 && window.scrollY > 210) {
+        delivery.classList.remove("delivery-address");
+        field.classList.remove("delivery-address-field");
+        fix.classList.remove("delivery-address-fixed");
+        
+        delivery.classList.add("delivery-active");
+        field.classList.add("field-active");
+        fix.classList.add("fixed-active");
+        console.log("if if if  telefon");
+      } else {
+        delivery.classList.add("delivery-address");
+        field.classList.add("delivery-address-field");
+        fix.classList.add("delivery-address-fixed");
   
-  window.addEventListener("scroll", activeDelivery);
+        delivery.classList.remove("delivery-active");
+        field.classList.remove("field-active");
+        fix.classList.remove("fixed-active");
+        console.log("else telefon");
+      }
+
+      if (screenWidth >= 1200 && window.scrollY > 10) {
+        navbar.classList.add("navbar-fullscreen");
+        console.log('Ekran o szerokości min. 1200px');
+
+
+      }else {
+        navbar.classList.remove("navbar-fullscreen");
+        console.log('Ekran o szerokości mniejszej niż 1200px');
+      }
+
+    }
+
+
+
+
+    window.addEventListener('scroll', checkScreenWidth);
+
+   window.addEventListener('resize', checkScreenWidth);
